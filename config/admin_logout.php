0b62081d68ff432ada8da3ec7d964ea31506bbfe
ardikabs <ardikabs@gmail.com>
2017-07-06 01:54:06 +0700

    minor change

--- config/admin_logout.php
+++ config/admin_logout.php
@@ -2,10 +2,6 @@
    
    if(session_destroy()) {
      unset($_SESSION['admin_session']);
-      ?>
-        <script type="text/javascript">
-        window.location.href = 'http://localhost:8080/gravijobs';
-        </script>
-      <?php
+     header("Location:http://jobs.gravicodev.id");
    }
 ?>
