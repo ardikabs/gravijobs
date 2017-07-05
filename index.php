041ab5c9391c6527cf6e07ee8df23b137baff030
ardikabs <ardikabs@gmail.com>
2017-07-05 21:56:26 +0700

    Update index.php

--- index.php
+++ index.php
@@ -5,7 +5,7 @@
 	<meta charset="utf-8" />
   <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1" />
   <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0" />
-  <meta name="description" content="Layanan cloud DDS Telkom" />
+  <meta name="description" content="Layanan Pekerjaan Mahasiswa" />
 	
 	<!-- Site Properties -->
 	<title>GRAVIJOBS</title>
