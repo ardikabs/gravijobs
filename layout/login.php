9fb6db5fa7289647f5c2825f8eaf718696d14236
ardikabs <ardikabs@gmail.com>
2017-07-06 01:45:01 +0700

    minor change

--- layout/login.php
+++ layout/login.php
@@ -22,14 +22,6 @@ if(isset($_POST['login']))
 
     if ($qUser->rowCount() > 0){
         if (password_verify($pass, $userRow['password'])){
-            if(!empty($userInfo)){
-
-                $_SESSION['id_lembaga'] = $userInfo['id_lembaga'];
-                $_SESSION['user_name'] = $userInfo['nama_lengkap'];
-                $_SESSION['user_addr'] = $userInfo['alamat'];
-                $_SESSION['user_phone'] = $userInfo['nomor_hp'];
-                $_SESSION['user_id_number'] = $userInfo['nomor_identitas'];
-            }
 
             if($userRow['id_role'] == 1){
                 $_SESSION['admin_session'] = $userRow['id_user'];
@@ -42,6 +34,14 @@ if(isset($_POST['login']))
             <?php
             }
             else if($userRow['id_role'] == 2 and $userRow['valid'] == 1 and $userRow['status'] == 1){
+                if(!empty($userInfo)){
+                    $_SESSION['id_lembaga'] = $userInfo['id_lembaga'];
+                    $_SESSION['user_name'] = $userInfo['nama_lengkap'];
+                    $_SESSION['user_addr'] = $userInfo['alamat'];
+                    $_SESSION['user_phone'] = $userInfo['nomor_hp'];
+                    $_SESSION['user_id_number'] = $userInfo['nomor_identitas'];
+                }
+
                 $_SESSION['user_session'] = $userRow['id_user'];
                 ?>
                 <script type="text/javascript">
