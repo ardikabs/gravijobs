b6f5b14b25fb85751a85045b02e478dfded569ab
ardikabs <ardikabs@gmail.com>
2017-07-05 23:45:39 +0700

    minor changes

--- manage/layout/lowongan.php
+++ manage/layout/lowongan.php
@@ -634,8 +634,14 @@
       formatter:{
         date: function(date, settings){
           if(!date) return '';
-          var day = date.getDate();
-          var month = date.getMonth();
+          var day = date.getDate() + '';
+          if (day.length < 2) {
+              day = '0' + day;
+          }
+          var month = (date.getMonth() + 1) + '';
+          if (month.length < 2) {
+              month = '0' + month;
+          }
           var year = date.getFullYear();
           return year+'-'+month+'-'+day;
         }
